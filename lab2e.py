2de03e4c070c49e35baf4fadf91628d765381369
Marc Esmer Palaypay <mepalaypay1@myseneca.ca>
2026-05-22 07:17:58 -0400

    Added lab2e

---
+++ lab2e.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#Author: Marc Esmer Palaypay
+#Author ID: mepalaypay1
+#Date Created: 05/22/2026
+
+timer=10
+while timer !=0:
+	print(timer)
+	timer = timer -1
+print('blast off!')
